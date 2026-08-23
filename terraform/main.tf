64c2d1be90ffd7c6b2724f5e8e66adc4a27cac73
tarun <tarunemail@example.com>
2026-08-23 19:35:32 +0530

    Configured Terraform S3 remote state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "aws-serverless-task-api-tfstate-2026"
+    key    = "aws-serverless-task-api/dev/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
